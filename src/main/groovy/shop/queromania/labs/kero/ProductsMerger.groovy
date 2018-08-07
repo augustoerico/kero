664a42e002005265df4f20a9b26a543436cd60f3
Erico Silva <augusto.ericosilva@gmail.com>
2018-08-06 21:33:25 -0400

    Fix merger update logic

--- src/main/groovy/shop/queromania/labs/kero/ProductsMerger.groovy
+++ src/main/groovy/shop/queromania/labs/kero/ProductsMerger.groovy
@@ -17,15 +17,27 @@ class ProductsMerger {
     }
 
     Map merge() {
-        def merged = [:]
-        def fetched = new JsonSlurper().parse(new File(paths.fetched)) as Map
-        new JsonSlurper().parse(new File(paths.exported)).each { String k, v ->
-            if (k in fetched.keySet()) {
-                merged[k] = (fetched.get(k) + v + [display: true]) as Map
+        Map<String, Map> merged = [:]
+        Map<String, Map> fetched = new JsonSlurper().parse(new File(paths.fetched)) as Map<String, Map>
+        Map<String, Map> exported = new JsonSlurper().parse(new File(paths.exported)) as Map<String, Map>
+
+        (fetched.keySet() + exported.keySet()).collect {
+            String key = it.toString()
+            if (key in exported.keySet() && key in fetched.keySet()) {
+                def update = [
+                        price        : fetched.get(key).price,
+                        discountPrice: fetched.get(key).discountPrice,
+                        display      : fetched.get(key).display
+                ] as Map
+                merged[key] = fetched.get(key) + exported.get(key) + update
+            } else if (key in exported.keySet()) {
+                def update = [display: false]
+                merged[key] = (exported.get(key) as Map) + update
             } else {
-                merged[k] = (v + [display: false]) as Map
+                merged[key] = fetched.get(it)
             }
         }
+
         return merged
     }
 }
