a5d06cf3214fc1dabb855ebe36ad2fc0af51bb7d
Erico Silva <augusto.ericosilva@gmail.com>
2019-03-02 20:07:21 -0500

    Add function to get product from url

--- src/main/groovy/shop/queromania/labs/crawlers/Crawler.groovy
+++ src/main/groovy/shop/queromania/labs/crawlers/Crawler.groovy
@@ -11,8 +11,35 @@ class Crawler {
         }
     }
 
+    static Map getProduct(String productUrl) {
+        def pageNode = Jsoup.connect(productUrl).get()
+        def contentNode = pageNode.select('div.entry-content')?.first()
+        def descriptionNode = contentNode.select('div.descriptions')?.first()
+
+
+        [
+                url        : productUrl,
+                sku        : descriptionNode.select('span').first().text().find(~'\\w\\d{5}'),
+                name       : contentNode?.select('h1.entry-title')?.first()?.text(),
+                description: descriptionNode?.select('p')?.first()?.text(),
+                exerpt     : contentNode?.select('p.excerpt')?.first()?.text(),
+                imageLinks : contentNode?.select('div.images')?.first()
+                        ?.select('a')?.collect { it.attr('href') },
+                variants   : [
+                        colors: contentNode?.select('div.cores')?.first()
+                                ?.select('a.field-color')?.collect { it.text() },
+                ],
+                taxonomy   : pageNode?.select('ul#menu-principal')?.first()
+                        ?.select('li.current-menu-parent > a')?.text()
+
+        ]
+    }
+
     static main(args) {
-        def url = 'http://demillus.vestemuitomelhor.com.br/?s=63577'
-        println(getLinksFromSearch(url))
+        def searchUrl = 'http://demillus.vestemuitomelhor.com.br/?s=63577'
+        println(getLinksFromSearch(searchUrl))
+
+        def productUrl = 'http://demillus.vestemuitomelhor.com.br/pecas/colecao-idylle-2/#main'
+        println(getProduct(productUrl))
     }
 }
